ddfea7ba052b2775c7d955de942034cf321d6b2d
bsramosl <steeven10f@gmail.com>
2021-04-28 16:54:00 -0500

    28/03

---
+++ static/comparar.js
@@ -0,0 +1,135 @@
+function grafica(t, vf, v0) {
+    console.log(v0)
+    var tiempo = t * 60;
+    var ace = vf - v0 / tiempo;
+    var velocidad = [];
+    var ti = [];
+    for (var i = 0; i <= tiempo; i++) {
+        ve = (parseFloat(ace) * i) + parseFloat(v0);
+        ti.push(i);
+        velocidad.push(ve);
+    }
+    console.log(velocidad)
+    graf(velocidad, ti);
+
+}
+
+function graf(datat, ti) {
+    let chart = Highcharts.chart('batch', {
+        title: {
+            text: 'Reactor Batch'
+        },
+
+        yAxis: {
+            title: {text: 'Velocidad'}
+        },
+
+        xAxis: {
+            title: {text: 'Tiempo'},
+            categorías: ti
+        },
+
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 0
+            }
+        },
+
+        series: [{
+            name: 'Velocidad',
+            data: datat
+        }],
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+
+}
+
+
+function graficatiempo(tb, x, v, umax,so,sf,x,y) {
+    X0 = x / v
+    dsf = X0 * Math.exp(umax * tb)
+
+    porsentcons = (so - sf* 100) / so
+    consumo = (so * 70) / 100
+    tbtiempo = 1 / umax
+    X0 = ( x / v)
+    tbtiempo1 = parseFloat(y / X0)
+    tbtiempo2 = parseFloat(1 + tbtiempo1 * (so - (so - consumo)))
+    tbtiempo4 = parseFloat(tbtiempo * Math.log(tbtiempo2)).toFixed(2)
+
+    dsft = X0 * Math.exp(umax * tbtiempo4)
+
+    var tiempo = [];
+    var densidad = [];
+    tiempo.push(tb);
+    tiempo.push(dsf);
+    densidad.push(tbtiempo4);
+    densidad.push(dsft);
+    if (tiempo.length != 0) {
+        grafti(densidad, tiempo);
+    }
+
+
+}
+
+function grafti(datat, ti) {
+    let chart = Highcharts.chart('predicctiempo', {
+        title: {
+            text: 'Reactor Batch'
+        },
+
+        yAxis: {
+            title: {text: 'Densidad'}
+        },
+
+        xAxis: {
+            title: {text: 'Tiempo'}
+        },
+
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 0
+            }
+        },
+        series: [{
+            name: 'Densidad',
+            data: [ti, datat]
+        }],
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+
+}
