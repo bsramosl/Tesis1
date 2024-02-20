d42121b4f80669668bc782ed03705038ce47726c
bsramosl <steeven10f@gmail.com>
2024-02-20 16:38:54 -0500

    dev

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qlka+=1h&lf^^(!q4vh-*oj)q9hky7adrxhc93=)^-a-rm@a+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
