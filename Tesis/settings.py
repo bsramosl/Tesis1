74b8be2a9f6a8d61bf32e333a84382d899e6a5d6
bsramosl <steeven10f@gmail.com>
2024-02-20 16:43:28 -0500

    sta

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 LOGOUT_REDIRECT_URL = '/Login/'
 MEDIA_URL = '/media/'
